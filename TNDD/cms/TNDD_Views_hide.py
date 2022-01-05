89d04a8a8519f72b71c0d26318ed558a4dd45b92
indiamcq <india.mcq@gmail.com>
2022-01-05 20:42:53 +1000

    Updated CMS and py files with no default options that can be changed by other cms tools.

---
+++ TNDD/cms/TNDD_Views_hide.py
@@ -0,0 +1,16 @@
+# Hide TNDD Views for Paratext
+# Written by Ian McQuay, SIL International, 2022-01-05+10:00
+
+# Import OS to be able to output correctly to Windows
+import os
+
+# Define batch files
+actionbatchfile = "C:\Users\Public\PT-Views\user-views-action.cmd"
+batchfile = "C:\Users\Public\PT-Views\user-views-manager.cmd"
+# setup variables for the ACTION batch file.
+commandstring = "echo set action=hide> " + actionbatchfile
+os.system(commandstring)
+commandstring = "echo set matchstart=TNDD>> " + actionbatchfile
+os.system(commandstring)
+# Run the batch file to do the action
+os.startfile(batchfile)
--- TNDD/cms/TNDD_Views_update.py
+++ TNDD/cms/TNDD_Views_update.py
@@ -1,20 +1,20 @@
-# TNDD Views Update for Paratext
-# Written by Ian McQuay, SIL International, 2019-12-10 
+# Update TNDD Views for Paratext
+# Written by Ian McQuay, SIL International, 2022-01-05+10:00
 
-# Import OS to be able to output correctly to Windows 
+# Import OS to be able to output correctly to Windows
 import os
 
 # Define batch files
 actionbatchfile = "C:\Users\Public\PT-Views\user-views-action.cmd"
 batchfile = "C:\Users\Public\PT-Views\user-views-manager.cmd"
-# setup variables for the ACTION batch file and write two variables
-commandstring = "echo set action=updateviews > " + actionbatchfile
+# setup variables for the ACTION batch file.
+commandstring = "echo set action=update> " + actionbatchfile
 os.system(commandstring)
-commandstring = "echo set matchstart=" + matchstart + ">> " + actionbatchfile
+commandstring = "echo set matchstart=TNDD>> " + actionbatchfile
 os.system(commandstring)
-commandstring = "echo set baseurl=" + baseurl + ">> " + actionbatchfile
+commandstring = "echo set baseurl=https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/TNDD>> " + actionbatchfile
 os.system(commandstring)
-commandstring = "echo set filelist=" + filelist + ">> " + actionbatchfile
+commandstring = "echo set filelist=TNDD-1st-mng-line.xslt TNDD-2nd-mng-line.xslt TNDD-3rd-mng-line.xslt TNDD-table-view.xslt TNDD-tag-errors.xslt TNDD-word-count-1st-mng-line.xslt TNDD-word-count-2nd-mng-line.xslt>> " + actionbatchfile
 os.system(commandstring)
 # Run the batch file to do the action
 os.startfile(batchfile)
