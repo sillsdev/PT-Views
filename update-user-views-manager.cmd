9a43587fd60169a6ec2d098a924e6575fa1ae287
indiamcq <india.mcq@gmail.com>
2024-01-23 09:03:28 +1000

    Create update-user-views-manager.cmd needed to update the user-views-manager.cmd file
    user-views-manager.cmd needed a switch added for curl to work.

---
+++ update-user-views-manager.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo Updating the user-views-manager.cmd from SIL source file.
+echo The file being replaced is renamed "user-views-manager.cmd.old"
+echo.
+if exist "C:\Users\Public\PT-Views\user-views-manager.cmd.old" del "C:\Users\Public\PT-Views\user-views-manager.cmd.old"
+ren "C:\Users\Public\PT-Views\user-views-manager.cmd" user-views-manager.cmd.old
+call curl https://raw.githubusercontent.com/sillsdev/PT-Views/master/user-views-manager.cmd --ssl-no-revoke > "C:\Users\Public\PT-Views\user-views-manager.cmd"
+echo.
+if exist "C:\Users\Public\PT-Views\user-views-manager.cmd" echo Update complete.
+pause
