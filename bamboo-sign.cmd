e7ac63820d5fccae94a64c293aff38c803d5eae4
indiamcq <ian_mcquay@sil.org>
2024-04-17 11:31:06 +1000

    testing batch to sign

---
+++ bamboo-sign.cmd
@@ -0,0 +1,2 @@
+curl.exe --ssl-no-revoke -o Paratext-Bamboo-Views-unsigned.exe https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/Paratext-Bamboo-Views-unsigned.exe
+sign.exe *.exe
