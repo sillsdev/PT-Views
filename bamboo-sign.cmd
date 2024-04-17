8b3432b9ea8941799d2477ad245c1f29103087b9
indiamcq <ian_mcquay@sil.org>
2024-04-17 17:02:06 +1000

    changes to script

--- bamboo-sign.cmd
+++ bamboo-sign.cmd
@@ -1,6 +1,6 @@
 set sourcefile=Paratext-Bamboo-Views-unsigned.exe
 set signedfile=Paratext-Bamboo-Views-signed-installer.exe
 
-del *.exe
-curl.exe --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/%sourcefile%
+if exist *.exe del *.exe
+curl --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/%sourcefile%
 sign *.exe
