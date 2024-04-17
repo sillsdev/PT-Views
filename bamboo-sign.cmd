ecf278f187e16691f691ffffe3856bad00aa458d
indiamcq <ian_mcquay@sil.org>
2024-04-17 16:40:55 +1000

    added variables and del old files

--- bamboo-sign.cmd
+++ bamboo-sign.cmd
@@ -1,2 +1,6 @@
-curl.exe --ssl-no-revoke -o Paratext-Bamboo-Views-unsigned.exe https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/Paratext-Bamboo-Views-unsigned.exe
-sign.exe *.exe
+set sourcefile=Paratext-Bamboo-Views-unsigned.exe
+set signedfile=Paratext-Bamboo-Views-signed-installer.exe
+
+del *.exe
+curl.exe --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/%sourcefile%
+sign *.exe
