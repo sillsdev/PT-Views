3cbf787ac668160252d9d82f7142ddee656f7320
indiamcq <india.mcq@gmail.com>
2024-04-19 03:00:34 +1000

    Update bamboo-sign.cmd
    Had wrong second variable name

--- bamboo-sign.cmd
+++ bamboo-sign.cmd
@@ -1,5 +1,5 @@
 set sourcefile=Paratext-Bamboo-Views-unsigned.exe
-set signedfile=Paratext-Bamboo-Views-signed-installer.exe
+set targetfile=Paratext-Bamboo-Views-signed-installer.exe
 
 if exist *.exe del *.exe
 curl --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/%sourcefile%
