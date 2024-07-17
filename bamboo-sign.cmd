d16563c9a87806bfc0d46440377f72adcbf1ecc1
indiamcq <ian_mcquay@sil.org>
2024-07-17 15:01:51 +1000

    fixed wrong github owner

--- bamboo-sign.cmd
+++ bamboo-sign.cmd
@@ -2,5 +2,5 @@ set sourcefile=Paratext-Bamboo-Views-unsigned.exe
 set targetfile=Paratext-Bamboo-Views-signed-installer.exe
 
 if exist *.exe del *.exe
-curl --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/SILAsiaPub/PT-Views/master/Installers/%sourcefile%
+curl --ssl-no-revoke -o "%targetfile%" https://raw.githubusercontent.com/sillsdev/PT-Views/master/Installers/%sourcefile%
 sign *.exe
