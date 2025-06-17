90f5beba54e5f7a2addabf3abecd6e101894b56f
indiamcq <ian_mcquay@sil.org>
2025-06-17 23:42:29 +1000

    Removed TNND

--- TNND/Uninstall-TNND-Views.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set viewgrp=TNND
-if exist user-views-manager.cmd call user-views-manager.cmd uninstall %viewgrp% & exit
-if exist ..\user-views-manager.cmd ( 
-cd .. 
-call user-views-manager.cmd uninstall %viewgrp%
-cd %viewgrp%
-)
-
